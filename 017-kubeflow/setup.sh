495ce54dec881c8b130cfc55385d6180600222ce
Shoaib Burq <saburq@gmail.com>
2020-05-04 23:12:58 +0200

    update

--- 017-kubeflow/setup.sh
+++ 017-kubeflow/setup.sh
@@ -4,7 +4,7 @@ export PATH=$PATH:/Users/shoaib/code/aicasts/aicasts.com/017-kubeflow
 # Set KF_NAME to the name of your Kubeflow deployment. This also becomes the
 # name of the directory containing your configuration.
 # For example, your deployment name can be 'my-kubeflow' or 'kf-test'.
-export KF_NAME=kf-geodb
+export KF_NAME=kf-geodb-v1
 
 # Set the path to the base directory where you want to store one or more
 # Kubeflow deployments. For example, /opt/.
@@ -13,7 +13,7 @@ export BASE_DIR=/Users/shoaib/code/aicasts/aicasts.com/017-kubeflow/opt
 export KF_DIR=${BASE_DIR}/${KF_NAME}
 
 # Set the configuration file to use, such as the file specified below:
-export CONFIG_URI="https://raw.githubusercontent.com/kubeflow/manifests/v1.0-branch/kfdef/kfctl_k8s_istio.v1.0.1.yaml"
+export CONFIG_URI="https://raw.githubusercontent.com/kubeflow/manifests/v1.0-branch/kfdef/kfctl_k8s_istio.v1.0.2.yaml"
 
 # Generate and deploy Kubeflow:
 mkdir -p ${KF_DIR}
