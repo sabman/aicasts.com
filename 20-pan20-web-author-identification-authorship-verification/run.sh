b0964c5e21aa08704a114e9662a3e60ce6ceb565
Shoaib Burq <saburq@gmail.com>
2020-11-14 23:04:17 +0100

    input files

--- 20-pan20-web-author-identification-authorship-verification/run.sh
+++ 20-pan20-web-author-identification-authorship-verification/run.sh
@@ -2,6 +2,17 @@
 
 # baseline script is representing documents using bag-of-character-ngrams model that is TFIDF weighted, cosine similarity between each document pair in the calibration data set is  calculated.
 
+# resulting similarities are optimized, and projected through a simple rescaling operation, so that they can function as pseudo-probabilities, indiciating the likelihood that a document-pair is a same-author pair
+
+# Input Pairs path: 
+# datasets/pan20-authorship-verification-training-small/pan20-authorship-verification-training-small.jsonl
+
+# Input Truth path:
+# datasets/pan20-authorship-verification-training-small/pan20-authorship-verification-training-small-truth.jsonl
+
+# Test Pairs Path:
+# datasets/pan20-authorship-verification-test/truth.jsonl
+
 python pan20-verif-baseline.py \
     -input_pairs="datasets/pan20-authorship-verification-training-small/pairs.jsonl" \
     -input_truth="datasets/pan20-authorship-verification-training-small/truth.jsonl" \
