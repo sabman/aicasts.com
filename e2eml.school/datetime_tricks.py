3392f515b51fef3dadd2a7e89a27e5580214adfd
Shoaib Burq <saburq@gmail.com>
2021-06-22 23:58:23 +0200

    datetime combine

--- e2eml.school/datetime_tricks.py
+++ e2eml.school/datetime_tricks.py
@@ -1,3 +1,13 @@
 
 # https://e2eml.school/datetime_tricks.html
 # DateTime Object
+
+import datetime
+
+# (hours, minutes)
+start_time = datetime.time(7, 0)
+# (year, month, day)
+start_date = datetime.date(2015, 5, 1)
+# Create a datetime object
+start_datetime = datetime.datetime.combine(
+    start_date, start_time)
