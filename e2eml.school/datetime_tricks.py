0b37c2598c3eaec89ae4d852b3d0035699c842a0
Shoaib Burq <saburq@gmail.com>
2021-06-23 23:58:47 +0200

    datetime epoc

--- e2eml.school/datetime_tricks.py
+++ e2eml.school/datetime_tricks.py
@@ -11,3 +11,9 @@ start_date = datetime.date(2015, 5, 1)
 # Create a datetime object
 start_datetime = datetime.datetime.combine(
     start_date, start_time)
+
+
+# Number of seconds from 12:00 am, January 1, 1970, UTC
+# is a computer-friendly way to handle time.
+unix_epoch = timestamp(start_datetime)
+start_datetime = fromtimestamp(1457453760)
