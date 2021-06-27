4dde6679f7a8bb4a14f07a58d679a6aacbb83c69
Shoaib Burq <saburq@gmail.com>
2021-06-27 23:43:13 +0200

    start multiprocessing example

---
+++ e2eml.school/multiprocessing.py
@@ -0,0 +1,15 @@
+
+from multiprocessing import Pool
+import os
+import numpy as np
+
+def f(n):
+    return np.var(np.random.sample((n, n)))
+
+result_objs = []
+n = 1000
+
+with Pool(processes=os.cpu_count() - 1) as pool:
+    for _ in range(n):
+        result = pool.apply_async(f, (n,))
+        result_objs.append(result)
