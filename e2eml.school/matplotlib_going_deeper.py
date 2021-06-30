f699d0b57febe205dbf7e1c112467820ef33206c
Shoaib Burq <saburq@gmail.com>
2021-06-30 22:57:16 +0200

    matplotlib markers

--- e2eml.school/matplotlib_going_deeper.py
+++ e2eml.school/matplotlib_going_deeper.py
@@ -3,8 +3,8 @@ matplotlib.use("agg")
 import matplotlib.pyplot as plt
 import numpy as np
 
+x = np.linspace(-1, 1)
+y = x + np.random.normal(size=x.size)
+
 fig = plt.figure()
 ax = fig.gca()
-ax.plot(x, y)
-
-fig.savefig("my_new_plot.png")
