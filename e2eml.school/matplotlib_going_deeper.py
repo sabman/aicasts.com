065814d5ad59164d4937d98fc4f695d3a043b69c
Shoaib Burq <saburq@gmail.com>
2021-06-30 22:57:52 +0200

    matplotlib markers

--- e2eml.school/matplotlib_going_deeper.py
+++ e2eml.school/matplotlib_going_deeper.py
@@ -8,3 +8,12 @@ y = x + np.random.normal(size=x.size)
 
 fig = plt.figure()
 ax = fig.gca()
+
+# Change the size
+ax.scatter(x, y, s=80)
+
+# Make every marker a different size
+# The real power of the scatter() function somes out when we want to modify markers individually.
+
+sizes = (np.random.sample(size=x.size) * 10) ** 2
+ax.scatter(x, y, s=sizes)
