e344de1795aa3b5068e899f78175338763cd7dba
Shoaib Burq <saburq@gmail.com>
2021-07-03 23:59:53 +0200

    matplotlib ticks

--- e2eml.school/matplotlib_ticks.py
+++ e2eml.school/matplotlib_ticks.py
@@ -10,3 +10,6 @@ fig = plt.figure()
 ax = fig.gca()
 ax.plot(x, y)
 
+ax.tick_params(axis="x", labelsize=18, labelrotation=-60, labelcolor="turquoise")
+ax.tick_params(axis="y", labelsize=12, labelrotation=20, labelcolor="orange")
+
