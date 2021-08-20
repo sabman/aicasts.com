3fb7e616428b1c62df483c4d00e5993383d5ed8d
Shoaib Burq <saburq@gmail.com>
2021-08-20 23:27:21 +0200

    add download data

--- e2eml.school/211. Decision Trees with Python and Pandas/download_data.py
+++ e2eml.school/211. Decision Trees with Python and Pandas/download_data.py
@@ -1,3 +1,13 @@
+import datetime
+
 def download_data(verbose=True):
     harvard_stop_id = '70086'
     jfk_stop_id = '70086'
+    
+    start_time = datetime.time(7, 0)
+    end_time = datetime.time(10, 0)
+    start_date = datetime.date(2015, 5, 1)
+    end_date = datetime.date(2018, 5, 1)
+
+    TTravelURL
+
