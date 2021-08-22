d2b20451577e4647541e30065b1d3c82a7233923
Shoaib Burq <saburq@gmail.com>
2021-08-22 23:30:28 +0200

    cycle through times

--- e2eml.school/211. Decision Trees with Python and Pandas/download_data.py
+++ e2eml.school/211. Decision Trees with Python and Pandas/download_data.py
@@ -10,5 +10,20 @@ def download_data(verbose=True):
     end_date = datetime.date(2018, 5, 1)
 
     TTravelURL = "http://realtime.mbta.com/developer/api/v2.1/traveltimes"
-    TKey = "?api_key=wX9NwuHnZU2To07GmGR"
-    TFormat = "json"
+    TKey = "?api_key=wX9NwuHnZU2To07GmGR9uw"
+    TFormat = "&format=json"
+    from_stop = "&from_stop="+str(jfk_stop_id) 
+    to_stop = "&to_stop="+str(to_stop_id)
+
+    # cycle through all the days
+    i_day = 0
+    trips = []
+    while True:
+        check_date = start + datetime.timedelta(days=i_day)
+        if check_date > end_date:
+            break
+
+        from_time = datetime.datetime.combine(check_date, start_time)
+        to_time = datetime.datetime.combine(check_date, end_time)
+
+
