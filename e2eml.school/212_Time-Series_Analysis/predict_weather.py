495cfeaa91bc98701367966bba00fb3a1be3d4cb
Shoaib Burq <saburq@gmail.com>
2021-09-30 23:12:21 +0200

    read in data

--- e2eml.school/212_Time-Series_Analysis/predict_weather.py
+++ e2eml.school/212_Time-Series_Analysis/predict_weather.py
@@ -1,2 +1,12 @@
 weather_filename = "fort-lauderdale-beach-weather.csv"
 weather_file = open(weather_filename)
+weather_data = weather_file.read()
+# print(len(weather_data))
+# print(weather_data[:200])
+lines = weather_data.split("\n")
+print(lines[:5])
+labels = lines[0]
+values = lines[1:]
+print(labels)
+for i in range(10):
+    print(values[i])
