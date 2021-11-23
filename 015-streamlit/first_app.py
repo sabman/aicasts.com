c7cdbd8d8a725cf6e8b8b87093ef3bf0b389532f
Shoaib Burq <saburq@gmail.com>
2021-11-24 03:11:13 +0500

    Update first_app.py

--- 015-streamlit/first_app.py
+++ 015-streamlit/first_app.py
@@ -1,3 +1,4 @@
+import time
 import streamlit as st
 # To make things easier later, we're also importing numpy and pandas for
 # working with sample data.
@@ -14,15 +15,15 @@ Here's our first attempt at using data to create a table:
 """
 
 df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
 })
 
 df
 
 chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
 
@@ -34,20 +35,17 @@ st.map(map_data)
 
 if st.checkbox('Show dataframe'):
     chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c'])
 
     st.line_chart(chart_data)
 
 
 option = st.sidebar.selectbox(
     'Which number do you like best?',
-     df['first column'])
+    df['first column'])
 
 'You selected:', option
-
-
-import time
 
 
 'Starting a long computation...'
@@ -57,9 +55,9 @@ latest_iteration = st.empty()
 bar = st.progress(0)
 
 for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
 
 '...and now we\'re done!'
