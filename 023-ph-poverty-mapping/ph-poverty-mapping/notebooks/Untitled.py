bf34cc2bccda4ac05dbfd8e39632e98c4ca7e4e9
Shoaib Burq <saburq@gmail.com>
2021-12-22 22:23:14 +0100

    format

--- 023-ph-poverty-mapping/ph-poverty-mapping/notebooks/Untitled.py
+++ 023-ph-poverty-mapping/ph-poverty-mapping/notebooks/Untitled.py
@@ -4,14 +4,17 @@
 # In[1]:
 
 
+from numpy.linalg import norm
+from numpy import dot
+from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
 
 # In[6]:
 
 
-a = np.array([1,0,-1,6,8])
-b = np.array([0,11,4,7,6])
+a = np.array([1, 0, -1, 6, 8])
+b = np.array([0, 11, 4, 7, 6])
 
 
 # In[12]:
@@ -29,13 +32,10 @@ d
 # In[14]:
 
 
-from sklearn.metrics.pairwise import cosine_similarity
-
-
 # In[15]:
 
 
-cosine_similarity([a,b])
+cosine_similarity([a, b])
 
 
 # In[16]:
@@ -47,9 +47,6 @@ np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 # In[17]:
 
 
-from numpy import dot
-from numpy.linalg import norm
-
 cos_sim = dot(a, b)/(norm(a)*norm(b))
 
 
@@ -60,7 +57,3 @@ cos_sim
 
 
 # In[ ]:
-
-
-
-
